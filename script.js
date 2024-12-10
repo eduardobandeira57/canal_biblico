7b0470a4d0cd70af363c72320bcfe2f531e422a6
eduardobandeira57 <eduapp196@gmail.com>
2024-12-10 07:26:02 -0300

    Update script.js
    alterando script

--- script.js
+++ script.js
@@ -1,16 +1,274 @@
-document.getElementById("entrada").textContent="Olá seja bem vindo!  "+prompt("Qual seu nome")
+document.getElementById("nome").textContent="Olá seja bem vindo!  "+prompt("Qual seu nome?")
 
-function p1(){
-   var pro1= prompt("estudo poder da oração")
-    document.getElementById("resut").textContent=pro1
- 
+function para1(){
+        
+    document.getElementById("paralelo").style.marginTop = "-10px";
+
+    document.querySelector(".v1").textContent="3 E quatro animais grandes, diferentes uns dos outros, subiam do mar.  "
+    document.querySelector(".v0").textContent="Daniel 7 v 3 ao 7"
+    document.querySelector(".v2").textContent="4 O primeiro era como leão, e tinha asas de águia; enquanto eu olhava, foram-lhe arrancadas as asas, e foi levantado da terra, e posto em pé como um homem, e foi-lhe dado um coração de homem. "
+    document.querySelector(".v3").textContent=" 5 Continuei olhando, e eis aqui o segundo animal, semelhante a um urso, o qual se levantou de um lado, tendo na boca três costelas entre os seus dentes; e foi-lhe dito assim: Levanta-te, devora muita carne.  "
+    document.querySelector(".v4").textContent="6 Depois disto, eu continuei olhando, e eis aqui outro, semelhante a um leopardo, e tinha quatro asas de ave nas suas costas; tinha também este animal quatro cabeças, e foi-lhe dado domínio."
+    document.querySelector(".v5").textContent="   7 Depois disto eu continuei olhando nas visões da noite, e eis aqui o quarto animal, terrível e espantoso, e muito forte, o qual tinha dentes grandes de ferro; ele devorava e fazia em pedaços, e pisava aos pés o que sobejava; era diferente de todos os animais que apareceram antes dele, e tinha dez chifres."
+    document.querySelector(".v6").textContent=""
+    document.querySelector(".apagar").textContent="<< Voltar"
+   
+};
+
+function busc(){
+      
 }
-function p2(){
-    var pro2=prompt("Usar seu tempo")
-    document.getElementById("resut").textContent=pro2
-}
-function p3(){
-    var pro2=prompt("preseverar na fé")
-    document.getElementById("resut").textContent=pro2
+
+
+
+
+function para2(){
+
+  document.getElementById("paralelo").style.marginTop = "50px";
+   document.querySelector(".v1").textContent="4 O primeiro era como leão, e tinha asas de águia; enquanto eu olhava, foram-lhe arrancadas as asas, e foi levantado da terra, e posto em pé como um homem, e foi-lhe dado um coração de homem."
+    document.querySelector(".v0").textContent="Dasniel 7; 4,5,6"
+    document.querySelector(".v2").textContent=" 5 Continuei olhando, e eis aqui o segundo animal, semelhante a um urso, o qual se levantou de um lado, tendo na boca três costelas entre os seus dentes; e foi-lhe dito assim: Levanta-te, devora muita carne."
+    document.querySelector(".v3").textContent="6 Depois disto, eu continuei olhando, e eis aqui outro, semelhante a um leopardo, e tinha quatro asas de ave nas suas costas; tinha também este animal quatro cabeças, e foi-lhe dado domínio."
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent="Lucas 4;6"
+    document.querySelector(".v6").textContent="6 E disse-lhe o diabo: Dar-te-ei a ti todo este poder e a sua glória; porque a mim me foi entregue, e dou-o a quem quero."
+    document.querySelector(".apagar").textContent="<< Voltar"
     
+  
 }
+function para3(){
+
+  document.getElementById("paralelo").style.marginTop = "80px";
+    document.querySelector(".v1").textContent=""
+    document.querySelector(".v0").textContent=""
+    document.querySelector(".v2").textContent=""
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+    document.querySelector(".apagar").textContent="<< Voltar"
+
+} 
+function para4(){
+  document.getElementById("paralelo").style.marginTop = "130px";
+  document.querySelector(".v1").textContent=" "
+    document.querySelector(".v0").textContent=""
+    document.querySelector(".v2").textContent=""
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+    document.querySelector(".apagar").textContent="<< Voltar"
+
+}
+function para5(){
+  document.getElementById("paralelo").style.marginTop = "160px";
+  document.querySelector(".v1").textContent="8 Estando eu a considerar os chifres, eis que, entre eles subiu outro chifre pequeno, diante do qual três dos primeiros chifres foram arrancados; e eis que neste chifre havia olhos, como os de homem, e uma boca que falava grandes coisas."
+    document.querySelector(".v0").textContent="Daniel 7"
+    document.querySelector(".v2").textContent="9 Eu continuei olhando, até que foram postos uns tronos, e um ancião de dias se assentou; a sua veste era branca como a neve, e o cabelo da sua cabeça como a pura lã; e seu trono era de chamas de fogo, e as suas rodas de fogo ardente."
+    document.querySelector(".v3").textContent="10 Um rio de fogo manava e saía de diante dele; milhares de milhares o serviam, e milhões de milhões assistiam diante dele; assentou-se o juízo, e abriram-se os livros."
+    document.querySelector(".v4").textContent="11 Então estive olhando, por causa da voz das grandes palavras que o chifre proferia; estive olhando até que o animal foi morto, e o seu corpo desfeito, e entregue para ser queimado pelo fogo;"
+    document.querySelector(".v5").textContent="2 Tessalonicenses 2"
+    document.querySelector(".v6").textContent="4 O qual se opõe, e se levanta contra tudo o que se chama Deus, ou se adora; de sorte que se assentará, como Deus, no templo de Deus, querendo parecer Deus."
+    document.querySelector(".apagar").textContent="<< Voltar"
+
+}
+function para6(){
+  document.getElementById("paralelo").style.marginTop = "190px";
+  document.querySelector(".v1").textContent="25 E proferirá palavras contra o Altíssimo, e destruirá os santos do Altíssimo, e cuidará em mudar os tempos e a lei; e eles serão entregues na sua mão, por um tempo, e tempos, e a metade de um tempo. "
+    document.querySelector(".v0").textContent="Daniel 7"
+    document.querySelector(".v2").textContent=""
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+    document.querySelector(".apagar").textContent="<< Voltar"
+
+}
+function para7(){
+  document.getElementById("paralelo").style.marginTop = "430px";
+  document.querySelector(".v1").textContent=""
+    document.querySelector(".v0").textContent="Daniel 7"
+    document.querySelector(".v2").textContent="21 Eu olhava, e eis que este chifre fazia guerra contra os santos, e prevaleceu contra eles."
+    document.querySelector(".v3").textContent="Apocalipse 11"
+    document.querySelector(".v4").textContent="3 E darei poder às minhas duas testemunhas, e profetizarão por mil duzentos e sessenta dias, vestidas de saco."
+    document.querySelector(".v5").textContent="7 E, quando acabarem o seu testemunho, a besta que sobe do abismo lhes fará guerra, e os vencerá, e os matará."
+    document.querySelector(".v6").textContent=""
+
+
+  document.querySelector(".apagar").textContent="<< Voltar"
+
+}
+function para8(){
+  document.getElementById("paralelo").style.marginTop = "600px";
+  document.querySelector(".v1").textContent="28 Sejam riscados do livro dos vivos, e não sejam inscritos com os justos."
+    document.querySelector(".v0").textContent="Salmos 69"
+    document.querySelector(".v2").textContent="Atos 2"
+    document.querySelector(".v3").textContent="23 A este que vos foi entregue pelo determinado conselho e presciência de Deus, prendestes, crucificastes e matastes pelas mãos de injustos;"
+    document.querySelector(".v4").textContent="1 Pedro 1"
+    document.querySelector(".v5").textContent="19 Mas com o precioso sangue de Cristo, como de um cordeiro imaculado e incontaminado,"
+    document.querySelector(".v6").textContent="20 O qual, na verdade, em outro tempo foi conhecido, ainda antes da fundação do mundo, mas manifestado nestes últimos tempos por amor de vós;"
+  document.querySelector(".apagar").textContent="<< Voltar"
+
+
+}
+function para9(){
+  document.getElementById("paralelo").style.marginTop = "800px";
+  document.querySelector(".v1").textContent=""
+    document.querySelector(".v0").textContent=""
+    document.querySelector(".v2").textContent=""
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+
+  document.querySelector(".apagar").textContent="<< Voltar" 
+}
+function para10(){
+  document.getElementById("paralelo").style.marginTop = "900px";
+  document.querySelector(".v1").textContent="6 Quem derramar o sangue do homem, pelo homem o seu sangue será derramado; porque Deus fez o homem conforme a sua imagem."
+    document.querySelector(".v0").textContent="Gênesis 9"
+    document.querySelector(".v2").textContent="Isaías 33"
+    document.querySelector(".v3").textContent="1 Ai de ti, despojador, que não foste despojado, e que procedes perfidamente contra os que não procederam perfidamente contra ti! Acabando tu de despojar, serás despojado; e, acabando tu de tratar perfidamente, perfidamente te tratarão."
+    document.querySelector(".v4").textContent="Jeremias 15"
+    document.querySelector(".v5").textContent="2 E será que, quando te disserem: Para onde iremos? Dir-lhes-ás: Assim diz o Senhor: Os que para a morte, para a morte, e os que para a espada, para a espada; e os que para a fome, para a fome; e os que para o cativeiro, para o cativeiro."
+    document.querySelector(".v6").textContent=""
+
+  document.querySelector(".apagar").textContent="<< Voltar" 
+}
+function para11(){
+  document.getElementById("paralelo").style.marginTop = "1100px";
+  document.querySelector(".v1").textContent=""
+    document.querySelector(".v0").textContent=""
+    document.querySelector(".v2").textContent=""
+    document.querySelector(".v3").textContent=" "
+    document.querySelector(".v4").textContent=" "
+    document.querySelector(".v5").textContent="  "
+    document.querySelector(".v6").textContent=""
+
+  document.querySelector(".apagar").textContent="<< Voltar" 
+}
+function para12(){
+  document.getElementById("paralelo").style.marginTop = "1200px";
+  document.querySelector(".v1").textContent=""
+    document.querySelector(".v0").textContent=""
+    document.querySelector(".v2").textContent=""
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+
+  document.querySelector(".apagar").textContent="<< Voltar" 
+}
+function para13(){
+  document.getElementById("paralelo").style.marginTop = "1300px";
+  document.querySelector(".v1").textContent=""
+    document.querySelector(".v0").textContent=""
+    document.querySelector(".v2").textContent=""
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+
+  document.querySelector(".apagar").textContent="<< Voltar" 
+}
+function para14(){
+  document.getElementById("paralelo").style.marginTop = "1400px";
+  document.querySelector(".v1").textContent=""
+    document.querySelector(".v0").textContent=""
+    document.querySelector(".v2").textContent=""
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+
+  document.querySelector(".apagar").textContent="<< Voltar" 
+}
+function para15(){
+  document.getElementById("paralelo").style.marginTop = "1580px";
+  document.querySelector(".v1").textContent=""
+    document.querySelector(".v0").textContent=""
+    document.querySelector(".v2").textContent=""
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+
+  document.querySelector(".apagar").textContent="<<Voltar"
+}
+function para16(){
+  document.getElementById("paralelo").style.marginTop = "1680px";
+  document.querySelector(".v1").textContent=""
+    document.querySelector(".v0").textContent=""
+    document.querySelector(".v2").textContent=""
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+
+  document.querySelector(".apagar").textContent="<< Voltar" 
+}
+function para17(){
+  document.getElementById("paralelo").style.marginTop = "1780px";
+  document.querySelector(".v1").textContent=""
+    document.querySelector(".v0").textContent=""
+    document.querySelector(".v2").textContent=" "
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+
+  document.querySelector(".apagar").textContent="<< Voltar" 
+}
+function para18(){
+  document.getElementById("paralelo").style.marginTop = "1880px";
+  document.querySelector(".v1").textContent=""
+    document.querySelector(".v2").textContent=" "
+    document.querySelector(".v0").textContent=" "
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+
+  document.querySelector(".apagar").textContent="<< Voltar" 
+}
+function para19(){
+  document.getElementById("paralelo").style.marginTop = "1980px";
+  document.querySelector(".v1").textContent=""
+    document.querySelector(".v0").textContent=""
+    document.querySelector(".v2").textContent=""
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+
+  document.querySelector(".apagar").textContent="<< Voltar" 
+}
+function para20(){
+  document.getElementById("paralelo").style.marginTop = "2080px";
+  document.querySelector(".v1").textContent=""
+    document.querySelector(".v0").textContent=""
+    document.querySelector(".v2").textContent=""
+    document.querySelector(".v3").textContent=""
+    document.querySelector(".v4").textContent=""
+    document.querySelector(".v5").textContent=""
+    document.querySelector(".v6").textContent=""
+
+  document.querySelector(".apagar").textContent="<< Voltar" 
+}
+function apagar(){
+ 
+ document.querySelector(".v1").textContent=""
+ document.querySelector(".v0").textContent=""
+ document.querySelector(".v2").textContent=""
+ document.querySelector(".v3").textContent=""
+ document.querySelector(".v4").textContent=""
+ document.querySelector(".v5").textContent=""
+ document.querySelector(".v6").textContent=""
+ document.querySelector(".apagar").textContent=""
+
+
+
+
+}
+
