7564dfcf813b75fba54a50cd10e7823df0068d2d
eduardobandeira57 <eduapp196@gmail.com>
2024-12-09 23:10:39 -0300

    Add files via upload
    subindo html,js,css

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("entrada").textContent="Olá seja bem vindo!  "+prompt("Qual seu nome")
+function p1(){
+   var pro1= prompt("estudo poder da oração")
+    
+ alert(pro1)
+}
+function p2(){
+    alert("não perder tempo")
+}
+function p3(){
+    alert("preseverar na fé")
+}
